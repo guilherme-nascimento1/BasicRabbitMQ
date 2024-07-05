73512e1aca2610c61b8e200c27873c36d6592b7d
Guilherme <guihsn2007@hotmail.com>
2024-07-05 15:50:04 -0300

    Adicionar arquivos de projeto.

---
+++ Send/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+class Send {
+    public static void Main() {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel()) {
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            Console.WriteLine("Digite sua mensagem e aparte <ENTER>");
+
+            string mensagem = Console.ReadLine();
+
+            if (mensagem == "") {
+                return;
+            }
+
+            //string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(mensagem);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent {0}", mensagem);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
